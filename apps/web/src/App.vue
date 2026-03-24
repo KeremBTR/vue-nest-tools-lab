38635665dba0b6a4bdb3e1b6a397ae114d1698f7
KeremBTR <94068893+KeremBTR@users.noreply.github.com>
2026-03-24 21:52:53 +0300

    feat(web/src): imported router in main.ts and modified App.vue

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <h1>Hello App!</h1>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav>
+    <RouterLink to="/">
+      Go to Home
+    </RouterLink>
+    <RouterLink to="/login">
+      Go to Login
+    </RouterLink>
+    <RouterLink to="/profile/123">
+      Go to Profile
+    </RouterLink>
+    <RouterLink to="/directory">
+      Go to Directory
+    </RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
