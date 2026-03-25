01a7305f975990eff4b9a902066a0e535b9c53bc
KeremBTR <94068893+KeremBTR@users.noreply.github.com>
2026-03-25 18:35:06 +0300

    style(web/App): added margins to nav

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -3,16 +3,13 @@
 <template>
   <h1>Welcome to Vue Nest Labs Project!</h1>
   <nav>
-    <RouterLink :to="{ name: 'home' }">
+    <RouterLink :to="{ name: 'home' }" style="margin-right: 10px;">
       Go to Home
     </RouterLink>
-    <RouterLink :to="{ name: 'login' }">
+    <RouterLink :to="{ name: 'login' }" style="margin-right: 10px;">
       Go to Login
     </RouterLink>
-    <RouterLink :to="{ name: 'profile', params: { id: 123 } }">
-      Go to Profile
-    </RouterLink>
-    <RouterLink :to="{ name: 'directory' }">
+    <RouterLink :to="{ name: 'directory' }" style="margin-right: 10px;">
       Go to Directory
     </RouterLink>
   </nav>
