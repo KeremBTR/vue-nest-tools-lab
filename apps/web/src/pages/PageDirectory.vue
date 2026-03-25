5793a5b3dad25abcc63dcd132fd5844284e99e82
KeremBTR <94068893+KeremBTR@users.noreply.github.com>
2026-03-25 17:18:35 +0300

    feat(web/PageDirectory): you can now go to imaginary profiles from the directory page

--- apps/web/src/pages/PageDirectory.vue
+++ apps/web/src/pages/PageDirectory.vue
@@ -2,6 +2,13 @@
 
 <template>
   <h1>This is the Directory Page</h1>
+  <RouterLink :to="`/profile/${123}`">
+    Go to Profile 123
+  </RouterLink>
+  <br>
+  <RouterLink :to="`/profile/${456}`">
+    Go to Profile 456
+  </RouterLink>
 </template>
 
 <style scoped></style>
