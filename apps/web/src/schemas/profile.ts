7c71007a036b09b9994a5bc6d9d70cc36b173145
KeremBTR <94068893+KeremBTR@users.noreply.github.com>
2026-05-04 19:39:59 +0300

    feat(web/schemas): add zod schema for validation on profile creation form

---
+++ apps/web/src/schemas/profile.ts
@@ -0,0 +1,17 @@
+/* eslint-disable unused-imports/no-unused-vars */
+import * as z from 'zod'
+
+const createProfileSchema = z.object({
+  name: z
+    .string('Name must be a string')
+    .trim()
+    .min(2, 'Too short.')
+    .max(20, 'Cmon man, your name can\'t be that long...'),
+  bio: z
+    .string()
+    .trim()
+    .min(10, 'Don\'t be shy. Tell us more about you.')
+    .max(250, 'People ain\'t gonna read this... make it shorter.'),
+})
+
+export type CreateProfileInput = z.infer<typeof createProfileSchema>
